78317a480fef403c243961ca1a41191dac3aa14c
Mahmoud Magdy <mahmoud.magdy1255@gmail.com>
2019-07-12 21:42:52 -0700

    configsss

--- Modules/Config/Resources/lang/en/config.php
+++ Modules/Config/Resources/lang/en/config.php
@@ -1,4 +1,15 @@
 <?php
 
 return [
+
+	'configs' => 'Configurations',
+	'about' => 'About us',
+	'other' => 'Other',
+	'title' => 'Title',
+	'desc' => 'Description',
+	'logo' => 'Logo',
+	'background' => 'Background',
+	'phone' => 'Phone',
+	'address' => 'Address',
+
 ];
